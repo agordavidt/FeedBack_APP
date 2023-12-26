66565e0c0e9b010eaaf0d457e5b7d8cf7616ef0b
David Agor <agordavid14@gmail.com>
2023-12-26 10:01:15 +0100

    php output types

---
+++ feedback/01-output.php
@@ -0,0 +1,22 @@
+<?php
+// single line comment
+
+/*    Multiple 
+    line 
+    comments
+*/
+
+// how to print
+
+// echo - Output strings, numbers, html, etc
+
+// print- Works like echo, but can only take in a single argument
+
+// print_r() - Print single values and arrays
+
+// var_dump() - Returns more info like data type and length
+
+// var_export() - Similar to var_dump(). Outputs a string represenation
+// of a varible.
+
+var_dump(34);
