7f14ea53fc46832c4644b497c3449a85488cb405
David Agor <agordavid14@gmail.com>
2023-12-26 18:26:37 +0100

    output file update

--- 01-output.php
+++ 01-output.php
@@ -6,17 +6,18 @@
     comments
 */
 
-// how to print
-
 // echo - Output strings, numbers, html, etc
+//echo 123, "hello", '10.5';
 
 // print- Works like echo, but can only take in a single argument
+//print 34;
 
 // print_r() - Print single values and arrays
+//print_r([1,2,3,4,5]);
 
 // var_dump() - Returns more info like data type and length
+var_dump([3,4,5,8,9]);
+var_dump(true);
+var_dump("Hello");
+// var_export() - Similar to var_dump(). Outputs a string represenation of a varible.
 
-// var_export() - Similar to var_dump(). Outputs a string represenation
-// of a varible.
-
-var_dump(34);
