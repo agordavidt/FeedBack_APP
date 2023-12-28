6bb92ec0541c22c84bf147a64fff5186c357ba0a
David Agor <agordavid14@gmail.com>
2023-12-28 12:52:15 +0100

    functions, scope, anonymous and arrow functions

--- 5-functions.php
+++ 5-functions.php
@@ -7,15 +7,32 @@ function registerUser($email){
 }
 
 // call function
-registerUser("davidagort@gmail.com");
+// registerUser("davidagort@gmail.com");
 
 
-function sum($n1, $n2){
+function sum($n1 = 9, $n2 = 3){
     return $n1 + $n2;
 }
 
-echo sum(5, 7);
+// echo sum(5, 7);
 
+$unit = '2GB';
+function buyData($network){
+    global $unit;
+    echo "credit $network $unit";
+}
 
+// buyData("MTN");
+
+// Ananymous function
+
+$subtract = function($n1, $n2){
+    return $n1 - $n2;
+};
+
+// echo $subtract(45,10);
+
+$multiply = fn($n1, $n2) => $n1 * $n2; // arrow function
+echo $multiply(5, 3);
 
 ?>
