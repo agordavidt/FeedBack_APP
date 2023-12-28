bde69c3e95dddb8399cb88ad0781a949d56b029c
David Agor <agordavid14@gmail.com>
2023-12-28 08:08:07 +0100

    control flow in php

--- 3-conditionals.php
+++ 3-conditionals.php
@@ -1,2 +1,13 @@
 // conditionals are control structures
-// Types 
+// Types of comparisions
+/*
+    < Less than
+    > Greater than
+    <= Less than or equal to
+    >= Greater than or equal 
+    == Equal to/Less
+    === Identical to 
+    != Not equal to
+    !== Not Identical to    
+    
+*/
