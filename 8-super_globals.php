f3095635d0402d8f735cf326328436d6ed0501c8
David Agor <agordavid14@gmail.com>
2023-12-28 15:45:39 +0100

    super global variables

---
+++ 8-super_globals.php
@@ -0,0 +1,21 @@
+<?php 
+/** ----------------------- Superglobals ------------------------ */
+// Built in variables that are always available in all scopes
+/**
+ * $_GET - Contains information about variables passed through a URL or a form.
+ * $_POST - contains information about variables passed through a form.
+ * $_COOKIE - Contains information about variables passed through a cookie.
+ * $_SESSION - Contains information about variables passed through a session.
+ * $_SERVER - Contains information about the server environment.
+ * $_ENV - Contains information about he environment variables.
+ * $_FILES - contains infroamtion about files uploaded to the script.
+ * $_REQUEST - contains inforamtion about variables passed through the from or URL.
+ */
+
+
+
+ // var_dump($_SERVER);    // gives information about the server
+ 
+
+
+?>
