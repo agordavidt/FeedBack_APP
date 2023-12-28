076c9d2bb7177103959c83f186c649d7ae73179d
David Agor <agordavid14@gmail.com>
2023-12-28 08:48:23 +0100

    ifs, switch and tenary statements

--- 3-conditionals.php
+++ 3-conditionals.php
@@ -1,3 +1,4 @@
+<?php
 // conditionals are control structures
 // Types of comparisions
 /*
@@ -9,5 +10,81 @@
     === Identical to 
     != Not equal to
     !== Not Identical to    
-    
 */
+/* ------------------ if statements --------------------- */
+
+/*
+** if Statement Syntax
+** if (condition) {
+        // code to be executed if condition is true
+
+} 
+
+*/
+
+/*
+
+$age = 10;
+if($age > 18){
+    echo "$age years old can vote";
+} else{
+    echo "Sorry, $age years old cannot vote";
+}
+
+*/
+
+$t = date("H");
+
+// print according to time of day
+/* 
+if($t < 12){
+    echo "Good morning";
+}elseif($t < 17){
+    echo "Good afternoon";
+} 
+else{
+    echo "Good evening";
+}
+*/
+
+$posts = ['This is my first posts, Hurray!'];
+// empty funtion to check for content in an array
+// if(!empty($posts)) {
+//     echo 'Post is not empty';
+// } else{
+//     echo 'Post is empty';
+// }
+
+// echo !empty($posts) ? $posts[0] : 'No posts';
+
+// $firstPost = !empty($posts) ? $posts[0] : 'No Posts';
+
+// $firstPost = !empty($posts) ? $posts[0] : null;
+
+// $fistPost = $posts[0] ?? null;
+// echo $firstPost;
+
+
+// Switches
+
+$favColor = 'red';
+
+switch($favColor){
+    case 'red':
+        echo "Your favourite color is $favColor";
+        break;
+    case 'blue':
+        echo "Your favourite color is $favColor";
+        break;
+    case 'green':
+        echo "Your favourtie color is $favColor";
+        brea;
+    default:
+    echo "you don't have a favourite color";
+}
+
+
+
+
+
+?>
