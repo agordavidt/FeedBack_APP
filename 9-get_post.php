fcb8815fc4f7de017c1e515f3df85b6eee0042ae
David Agor <agordavid14@gmail.com>
2023-12-29 16:51:00 +0100

    GET method

--- 9-get_post.php
+++ 9-get_post.php
@@ -7,8 +7,27 @@
 
  // echo $_GET['name'];
 
- echo 'Hello ' . PHP_EOL . ' David';
+ //echo 'Hello ' . PHP_EOL . ' David';
+ echo $_GET['name'] . '<br>';
+ echo $_GET['age'];
+
+
+ // GET can be used for urls and forms
+ // POST can only be used for forms
 
 ?>
 
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>? name=John">Click</a>
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=John">Click</a>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+    <div>
+        <label for="name">Name: </label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label for="age">Age: </label>
+        <input type="text" name="age">
+    </div>
+    <input type="submit" value="submit" name="submit">
+
+</form>
