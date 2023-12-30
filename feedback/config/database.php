6f2859b5ad7704a8e78ca8e3a9357f5b36948665
David Agor <agordavid14@gmail.com>
2023-12-30 16:53:20 +0100

    connect to mysql server

---
+++ feedback/config/database.php
@@ -0,0 +1,19 @@
+<?php 
+define('DB_HOST', 'localhost');
+define('DB_USER','john');
+define('DB_PASS','123456');
+define('DB_NAME','self_dev');
+
+
+// create connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Check connection
+if($conn->connect_error) {
+    die('Connection Failed'. $conn->connect_error);
+
+}
+
+echo 'CONNECTED';
+
+?>
