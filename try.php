b88e9f20e443cb7a72c7fb5d3992c37c158049a5
David Agor <agordavid14@gmail.com>
2023-12-31 09:47:10 +0100

    update try.php

--- try.php
+++ try.php
@@ -26,10 +26,18 @@
     <h2>Feedback</h2>
 
     <?php if(!empty($source)): ?>
+      
         <p>There is a lot to share!</p>
     <?php endif; ?> 
 
-    <?php foreach($source as $details); ?>
-    <p><?php echo $details['team']; ?></p>
+    <?php foreach($source as $details): ?>
+        <div style="background-color: orange; color: darkgreen; border: solid 2px darkgreen; padding: 20px;
+        margin: 10px;">
+        <p><?php echo " Team: ${details['team']}"; ?></p>
+        <p><?php echo " Coach: ${details['coach']}"; ?></p>
+        <p><?php echo " Status: ${details['status']}"; ?></p>
+        </div>
+    
+    <?php endforeach; ?>
 
     
